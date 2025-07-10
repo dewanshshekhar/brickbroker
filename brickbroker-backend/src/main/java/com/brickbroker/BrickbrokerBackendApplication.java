50355280d7e907474498bce6da9840a85df9b5e9
DheerajSharma2002 <ds817897@gmail.com>
2025-07-10 17:29:09 +0530

    Initial monorepo commit with backend setup

---
+++ brickbroker-backend/src/main/java/com/brickbroker/BrickbrokerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.brickbroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickbrokerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrickbrokerBackendApplication.class, args);
+	}
+
+}
---
+++ brickbroker-backend/src/test/java/com/brickbroker/BrickbrokerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brickbroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrickbrokerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
